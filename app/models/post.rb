cfe29ed5332cb80f25673e00e7f6053047bc57a3
Parker Parham <parkerparham@gmail.com>
2022-12-08 15:40:01 -0500

    commit tree a3269e44630a3de99f25a8c2de9f059ccebeff6e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,36 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :clickbait?
+    # validate :must_be_fiction_or_nonfiction
+
+    #custom validations
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+            errors.add(:title, "must be clickbait")
+        end
+    end
+
+    # def must_be_fiction_or_nonfiction
+    #     if (category != "Fiction" && category != "Non-Fiction")
+    #         errors.add(:category, "is not included in the list")
+    #     end
+    # end
+
+    # # def must_be_fiction_or_nonfiction
+    # #     unless (category.downcase == "fiction" or category.downcase == "non-fiction")
+    # #         errors.add(:category, 'Category must be Fiction or Non-Fiction')
+    # #     end
+    # end
+
 end
